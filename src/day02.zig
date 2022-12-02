2cd1af299ee4921d9902afab1dbc7c3b1c49ab8f
Walther Chen <walther.chen@gmail.com>
2022-12-02 16:54:20 -0500

    day02: less data modeling, more clarity

--- src/day02.zig
+++ src/day02.zig
@@ -1,3 +1,10 @@
+//! Switching on the round instead of each side's move seems to be sweet spot in readability.
+//! Using arithmetic is harder to understand, switching on both moves creates more nesting
+//! which is harder to understand.
+//!
+//! If there were more variations, then using arithmetic would probably be better, with lots
+//! of comments explaining.
+
 const std = @import("std");
 const data = @embedFile("input/day02.txt");
 const expectEqual = std.testing.expectEqual;
@@ -18,95 +25,48 @@ fn run(input: []const u8) struct { part01: u64, part02: u64 } {
     var lines = std.mem.tokenize(u8, input, "\n");
 
     while (lines.next()) |line| {
-        var cols = std.mem.tokenize(u8, line, " ");
+        const round = std.meta.stringToEnum(Round, line).?;
 
-        const opp_move = Move.from_char(cols.next().?[0]);
-        const col_2 = cols.next().?[0];
+        // scores are result_score + move_score
+        part01 += switch (round) {
+            .@"A X" => 3 + 1,
+            .@"A Y" => 6 + 2,
+            .@"A Z" => 0 + 3,
+            .@"B X" => 0 + 1,
+            .@"B Y" => 3 + 2,
+            .@"B Z" => 6 + 3,
+            .@"C X" => 6 + 1,
+            .@"C Y" => 0 + 2,
+            .@"C Z" => 3 + 3,
+        };
 
-        // part 01
-        {
-            const my_move = Move.from_char(col_2);
-            // add score for winner
-            switch (my_move) {
-                .rock => switch (opp_move) {
-                    .rock => part01 += 3,
-                    .paper => {},
-                    .scissors => part01 += 6,
-                },
-                .paper => switch (opp_move) {
-                    .rock => part01 += 6,
-                    .paper => part01 += 3,
-                    .scissors => {},
-                },
-                .scissors => switch (opp_move) {
-                    .rock => {},
-                    .paper => part01 += 6,
-                    .scissors => part01 += 3,
-                },
-            }
-
-            // add score for move played
-            part01 += my_move.score();
-        }
-
-        // part 02
-        {
-            // col_2 is now the selected strategy
-            switch (col_2) {
-                'X' => {
-                    // lose:
-                    // get 0 points plus move score
-                    switch (opp_move) {
-                        .rock => part02 += Move.scissors.score(),
-                        .paper => part02 += Move.rock.score(),
-                        .scissors => part02 += Move.paper.score(),
-                    }
-                },
-                'Y' => {
-                    // draw:
-                    // get 3 points plus move score (which is same as opp move)
-                    part02 += opp_move.score() + 3;
-                },
-                'Z' => {
-                    // win:
-                    // get 6 points plus move score
-                    switch (opp_move) {
-                        .rock => part02 += Move.paper.score(),
-                        .paper => part02 += Move.scissors.score(),
-                        .scissors => part02 += Move.rock.score(),
-                    }
-
-                    part02 += 6;
-                },
-                else => unreachable,
-            }
-        }
+        // scores are result_score + move_score
+        part02 += switch (round) {
+            .@"A X" => 0 + 3,
+            .@"A Y" => 3 + 1,
+            .@"A Z" => 6 + 2,
+            .@"B X" => 0 + 1,
+            .@"B Y" => 3 + 2,
+            .@"B Z" => 6 + 3,
+            .@"C X" => 0 + 2,
+            .@"C Y" => 3 + 3,
+            .@"C Z" => 6 + 1,
+        };
     }
 
     return .{ .part01 = part01, .part02 = part02 };
 }
 
-const Move = enum {
-    rock,
-    paper,
-    scissors,
-
-    fn from_char(c: u8) Move {
-        return switch (c) {
-            'A', 'X' => .rock,
-            'B', 'Y' => .paper,
-            'C', 'Z' => .scissors,
-            else => unreachable,
-        };
-    }
-
-    fn score(self: Move) u64 {
-        return switch (self) {
-            .rock => 1,
-            .paper => 2,
-            .scissors => 3,
-        };
-    }
+const Round = enum {
+    @"A X",
+    @"A Y",
+    @"A Z",
+    @"B X",
+    @"B Y",
+    @"B Z",
+    @"C X",
+    @"C Y",
+    @"C Z",
 };
 
 test "part01" {
