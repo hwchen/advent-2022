2f65c899af69440e3466cd31f9fff0486406cd39
Walther Chen <walther.chen@gmail.com>
2022-12-03 19:55:01 -0500

    day03: part02

--- src/day03.zig
+++ src/day03.zig
@@ -14,8 +14,8 @@ fn run(input: []const u8) struct { part01: u64, part02: u64 } {
     var part01: u64 = 0;
     var part02: u64 = 0;
 
+    // part 01
     var lines = std.mem.tokenize(u8, input, "\n");
-
     while (lines.next()) |line| {
         var half_idx = line.len / 2;
         var rucksack_1 = line[0..half_idx];
@@ -39,10 +39,81 @@ fn run(input: []const u8) struct { part01: u64, part02: u64 } {
         }
     }
 
+    // part 02
+    // No alloc, nested loops. Is there a better way w/out alloc?
+    var groups = GroupsIterator{ .working_slice = input };
+    while (try groups.next()) |group| {
+        blk: {
+            var i: usize = 0;
+            while (i < group.ruck_0.len) : (i += 1) {
+                var j: usize = 0;
+                while (j < group.ruck_1.len) : (j += 1) {
+                    var k: usize = 0;
+                    while (k < group.ruck_2.len) : (k += 1) {
+                        if (group.ruck_0[i] == group.ruck_1[j] and group.ruck_1[j] == group.ruck_2[k]) {
+                            switch (group.ruck_0[i]) {
+                                'a'...'z' => part02 += group.ruck_0[i] - 96,
+                                'A'...'Z' => part02 += group.ruck_0[i] - 64 + 26,
+                                else => unreachable,
+                            }
+                            break :blk;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     return .{ .part01 = part01, .part02 = part02 };
 }
 
-test "part01" {
+const GroupsIterator = struct {
+    working_slice: []const u8,
+
+    /// Groups in input are exact, so don't handle errors.
+    fn next(self: *GroupsIterator) !?Group {
+        if (self.working_slice.len == 0) {
+            return null;
+        }
+
+        const end_0 = std.mem.indexOf(u8, self.working_slice, "\n").?;
+        const ruck_0 = self.working_slice[0..end_0];
+        self.working_slice = self.working_slice[end_0 + 1 ..];
+
+        const end_1 = std.mem.indexOf(u8, self.working_slice, "\n").?;
+        const ruck_1 = self.working_slice[0..end_1];
+        self.working_slice = self.working_slice[end_1 + 1 ..];
+
+        // Last line may not have `\n`.
+        const maybe_end_2 = std.mem.indexOf(u8, self.working_slice, "\n");
+        const ruck_2 = blk: {
+            if (maybe_end_2) |end| {
+                const r2 = self.working_slice[0..end];
+                self.working_slice = self.working_slice[end + 1 ..];
+                break :blk r2;
+            } else {
+                const r2 = self.working_slice[0..];
+                self.working_slice = self.working_slice[self.working_slice.len..];
+                break :blk r2;
+            }
+        };
+
+        const res = Group{
+            .ruck_0 = ruck_0,
+            .ruck_1 = ruck_1,
+            .ruck_2 = ruck_2,
+        };
+        return res;
+    }
+};
+
+const Group = struct {
+    ruck_0: []const u8,
+    ruck_1: []const u8,
+    ruck_2: []const u8,
+};
+
+test "test_day_03" {
     const input =
         \\vJrwpWtwJgWrhcsFMMfFFhFp
         \\jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
@@ -52,18 +123,7 @@ test "part01" {
         \\CrZsJsPPZsGzwwsLwLmpwMDw
     ;
 
-    try expectEqual(run(input).part01, 157);
-}
-
-test "part02" {
-    const input =
-        \\vJrwpWtwJgWrhcsFMMfFFhFp
-        \\jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
-        \\PmmdzqPrVvPwwTWBwg
-        \\wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
-        \\ttgJtRGJQctTZtZT
-        \\CrZsJsPPZsGzwwsLwLmpwMDw
-    ;
-
-    try expectEqual(run(input).part02, 0);
+    const out = run(input);
+    try expectEqual(out.part01, 157);
+    try expectEqual(out.part02, 70);
 }
